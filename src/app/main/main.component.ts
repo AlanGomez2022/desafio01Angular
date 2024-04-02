c90b612fdcf212b44014fffc13e269832b8edb9e
AlanGomez2022 <alanagustingomez@gmail.com>
2024-04-02 01:57:23 -0300

    Desafio 02 - listado alumnos

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class MainComponent {
 
+  curso = '';
+  muestra6to=false; 
+  alumnos6to=[
+    {dni:34943846, Nombre:'Alan', Apellido:'Gomez', asistencia:false},
+    {dni:34931090, Nombre:'Romina', Apellido:'Barragan', asistencia:true},
+    {dni:53587322, Nombre:'Catalina', Apellido:'Gomez', asistencia:false},
+    {dni:13218124, Nombre:'Marta', Apellido:'Maspero', asistencia:true},
+    {dni:13241711, Nombre:'Omar', Apellido:'Gomez', asistencia:false}
+  ]
+  mostrar6to(){
+    this.muestra6to=!this.muestra6to;
+    this.curso='6to año'
+  }
+
+  cambiarPresente(){
+
+  }
+  cambiarAusente(){
+
+  }
 }
